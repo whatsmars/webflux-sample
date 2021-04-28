6d7681d2292b429cc16b9191dac0f9ed766308ac
javahongxi <javahongxi@qq.com>
2021-04-28 14:42:47 +0800

    DefaultExceptionHandler

--- src/main/java/org/hongxi/sample/webflux/exception/ExceptionHandlerConfiguration.java
+++ src/main/java/org/hongxi/sample/webflux/exception/ExceptionHandlerConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.result.view.ViewResolver;
@@ -19,7 +18,7 @@ import java.util.stream.Collectors;
 public class ExceptionHandlerConfiguration {
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Order(-1)
     public ErrorWebExceptionHandler errorWebExceptionHandler(
             ErrorAttributes errorAttributes,
             WebProperties.Resources resources,
