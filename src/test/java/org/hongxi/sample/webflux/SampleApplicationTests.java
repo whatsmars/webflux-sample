ced4b725b26e225c6e2ccfbd175cad3dc55da0c8
javahongxi <javahongxi@qq.com>
2021-05-05 16:56:28 +0800

    test

--- src/test/java/org/hongxi/sample/webflux/SampleApplicationTests.java
+++ src/test/java/org/hongxi/sample/webflux/SampleApplicationTests.java
@@ -3,7 +3,6 @@ package org.hongxi.sample.webflux;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.hongxi.sample.webflux.model.Order;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
