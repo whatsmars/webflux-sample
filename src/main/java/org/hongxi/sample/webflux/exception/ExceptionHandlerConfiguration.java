432ac2e1c0eb678cfc11c99cb0241084daec82d8
javahongxi <javahongxi@qq.com>
2021-05-07 16:09:04 +0800

    ExceptionHandlerConfiguration

--- src/main/java/org/hongxi/sample/webflux/exception/ExceptionHandlerConfiguration.java
+++ src/main/java/org/hongxi/sample/webflux/exception/ExceptionHandlerConfiguration.java
@@ -1,8 +1,12 @@
 package org.hongxi.sample.webflux.exception;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.context.ApplicationContext;
@@ -10,15 +14,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.view.ViewResolver;
 
 import java.util.stream.Collectors;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+@ConditionalOnClass(WebFluxConfigurer.class)
+@AutoConfigureBefore(WebFluxAutoConfiguration.class)
 public class ExceptionHandlerConfiguration {
 
     @Bean
-    @Order(-1)
+    @Order(-2)
     public ErrorWebExceptionHandler errorWebExceptionHandler(
             ErrorAttributes errorAttributes,
             WebProperties.Resources resources,
