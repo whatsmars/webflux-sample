6e88d04aa6566683099cedde5afeecd938afe87c
javahongxi <javahongxi@qq.com>
2021-06-23 11:04:34 +0800

    优化

--- src/main/java/org/hongxi/sample/webflux/support/ModifiedServerHttpResponse.java
+++ src/main/java/org/hongxi/sample/webflux/support/ModifiedServerHttpResponse.java
@@ -44,25 +44,13 @@ public class ModifiedServerHttpResponse extends ServerHttpResponseDecorator {
         // 这里只是借用 ClientResponse 这个类获取修改之前的 body
         // server 端最终返回的是 ServerResponse/ServerHttpResponse
         ClientResponse clientResponse = prepareClientResponse(body, httpHeaders);
-        Mono<byte[]> modifiedBody = clientResponse.bodyToMono(byte[].class)
-                .flatMap(originalBody -> {
+        Mono<DataBuffer> modifiedBody = clientResponse.bodyToMono(byte[].class)
+                .map(originalBody -> {
                     this.body = new String(originalBody, StandardCharsets.UTF_8);
-                    return Mono.just(Crypto.encrypt(originalBody));
-                });
-
-        BodyInserter<Mono<byte[]>, ReactiveHttpOutputMessage> bodyInserter =
-                BodyInserters.fromPublisher(modifiedBody, byte[].class);
-        CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange,
-                exchange.getResponse().getHeaders());
-        return bodyInserter.insert(outputMessage, new BodyInserterContext()).then(Mono.defer(() -> {
-            Mono<DataBuffer> messageBody = DataBufferUtils.join(outputMessage.getBody());
-            HttpHeaders headers = getDelegate().getHeaders();
-            if (!headers.containsKey(HttpHeaders.TRANSFER_ENCODING)
-                    || headers.containsKey(HttpHeaders.CONTENT_LENGTH)) {
-                messageBody = messageBody.doOnNext(data -> headers.setContentLength(data.readableByteCount()));
-            }
-            return getDelegate().writeWith(messageBody);
-        }));
+                    return Crypto.encrypt(originalBody);
+                }).map(encrypted -> getDelegate().bufferFactory().wrap(encrypted))
+                .doOnNext(data -> getDelegate().getHeaders().setContentLength(data.readableByteCount()));
+        return super.writeWith(modifiedBody);
     }
 
     @Override
